1d95412651ced8b045b07cfc2c4f26f21aa495e0
thisissagnik <sagnik.tict@gmail.com>
2020-10-31 21:20:30 +0530

    commit 2nd problem

--- 1.Monk and Rotatation.py
+++ 1.Monk and Rotatation.py
@@ -1,32 +1,11 @@
-<<<<<<< HEAD
-def Rotate_Right(N,K,arr):
-    M=K%N
-    temp = arr[N-M:] + arr [:N-M]
-=======
-def Rotate_Right(N, K, arr) -> object:
+def Rotate_Right(N, K, arr):
     M = K % N
     temp = arr[N - M:] + arr[:N - M]
->>>>>>> commit 2nd problem
 
     return " ".join(str(x) for x in temp)
 
 
 def main():
-<<<<<<< HEAD
-    T= input()
-
-    for i in range(int(T)) :
-        N,K= map(int,input().split())
-
-        arr= list(map(int,input().split())) [:N]
-
-        print(Rotate_Right(N,K,arr))
-        #Rotate_Right(N,K,arr)
-
-main()
-
-
-=======
     T = input()
 
     for i in range(int(T)):
@@ -39,4 +18,3 @@ main()
 
 
 main()
->>>>>>> commit 2nd problem
